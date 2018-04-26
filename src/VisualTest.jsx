411a0e00799b557984d77d90ba04b193a71bc440
Jye Lewis <jye@jyelewis.com>
2018-04-26 18:46:09 +1000

    Mockup for form using uncontrolled elements, using material-ui for the sake of time

--- src/VisualTest.jsx
+++ src/VisualTest.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './VisualTest.css';
+
+import {Paper, Typography} from "material-ui";
+
 import ContactDetailsForm from "./components/ContactDetailsForm";
 
 class VisualTest extends Component {
     render() {
         return (
             <div className="component-VisualTest">
-                <ContactDetailsForm />
+                <Paper className="visual-test-paper">
+                    <Typography variant="title">
+                        Contact details
+                    </Typography>
+
+                    <ContactDetailsForm />
+                </Paper>
+
+                <Paper className="visual-test-paper">
+                    *Test output here*
+                </Paper>
             </div>
         );
     }
--- src/components/ContactDetailsFields.jsx
+++ src/components/ContactDetailsFields.jsx
@@ -1,9 +1,72 @@
 import React, { Component } from 'react';
+import {
+    TextField,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+    Checkbox,
+    FormControlLabel,
+    Button
+} from "material-ui";
 
 class ContactDetailsFields extends Component {
     render() {
         return (
-            <h1>Hello world!</h1>
+            <div className="component-ContactDetailsFields">
+                <TextField
+                    autoFocus
+                    fullWidth
+                    margin="normal"
+                    label="Name"
+                />
+
+                <TextField
+                    fullWidth
+                    margin="normal"
+                    label="Date of birth"
+                />
+
+                <FormControl fullWidth margin="normal">
+                    <InputLabel htmlFor="drp-gender">Gender</InputLabel>
+                    <Select
+                        id="drp-gender"
+                        value=""
+                    >
+                        <MenuItem value="">
+                            <em>None provided</em>
+                        </MenuItem>
+                        <MenuItem value="male">Male</MenuItem>
+                        <MenuItem value="female">Female</MenuItem>
+                    </Select>
+                </FormControl>
+
+                * TODO: contact details *
+                <br /><br />
+
+                <FormControlLabel
+                    control={
+                        <Checkbox />
+                    }
+                    label="Require guardian consent"
+                />
+
+                <TextField
+                    fullWidth
+                    margin="normal"
+                    label="Guardian name"
+                />
+
+                <TextField
+                    fullWidth
+                    margin="normal"
+                    label="Guardian contact number"
+                />
+
+                <Button>
+                    Submit
+                </Button>
+            </div>
         );
     }
 }
